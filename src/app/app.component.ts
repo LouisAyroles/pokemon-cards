97f188cd4e5e0a8fb9e3412988e0516e5c7b5d34
LouisAyroles <louis.ayroles@master-developpement-logiciel.fr>
2021-11-26 12:29:20 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pokemons';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pokemon-app',
+  templateUrl: `app.component.html`,
+  styleUrls: [`app.component.css`]
 })
-export class AppComponent {
-  title = 'pokemon-app';
+export class AppComponent implements OnInit {
+
+  public pokemons: Pokemon[];
+
+  ngOnInit(){
+    this.pokemons = POKEMONS;
+  }
+
+  selectPokemon(pokemon: Pokemon){
+    alert("Vous avez cliqué sur " + pokemon.name);
+  }
 }
